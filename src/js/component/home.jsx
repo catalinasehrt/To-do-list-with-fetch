fdd8db17cf9200a4b0bd566706d255b1190446e0
catalinasehrt <catalina.dali94@gmail.com>
2023-03-31 02:50:21 +0000

    ahora si tiene la api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,69 +8,77 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [tarea, setTarea] = useState([]);
 	
-	
-				
-				function putApi() {
-					var myHeaders = new Headers();
-					myHeaders.append("Content-Type", "application/json");
-					
-					var raw = JSON.stringify(tarea);
-					
-					var requestOptions = {
-					  method: 'PUT',
-					  headers: myHeaders,
-					  body: raw,
-					  redirect: 'follow'
-					};
-					
-					fetch("https://assets.breatheco.de/apis/fake/todos/user/catas", requestOptions)
-					  .then(response => response.text())
-					  .then(result => console.log(result))
-					  .catch(error => console.log('error', error));
+    const deleteTarea = (i) => {
+        setTarea(
+            tarea.filter((value, index) => {
+                return index != i;
+            })
+        );
+    };
 
-				}
-			
-			
-         useEffect(()=>{
-              fetch("https://assets.breatheco.de/apis/fake/todos/user/catas")
-             .then((response)=>response.json())
-              .then((data)=>Settarea(data));
-            },[]);
+    function putApi() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-             useEffect(()=>{ 
-             putApi();
-            },[tarea]);
+        var raw = JSON.stringify(tarea);
 
-			return (
-				<div className="text-center text-info m-5">
-					
-					<h1> ✨ Lista deberes 📝</h1> 
-		
-					<form onSubmit={(event) =>{
-						event.preventDefault ();
-						setTarea([...tarea,event.target[0].value])
-						event.target[0].value = "";
-		
-					}}>
-		
-		
-					<input placeholder="Nueva Tarea"/> 
-					</form>
-					<div className="list-group container col-4" >
-					{tarea.map ((value, index, arr) =>{
-						return <li key={index}>{value} <button onClick={()=>setTarea(tarea.filter((item, idx)=> idx !== index)  )}>🗑</button> </li>
-		
-						} )	
-						}
- 	
+        var requestOptions = {
+			method: 'GET',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/catalinadali", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+    }
 
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/catalinadali")
+            .then((respuesta) => respuesta.json())
+            .then((data) => setTarea(data));
+    }, []);
 
-			</div>
+    useEffect(() => {
+        putApi();
+    }, [tarea])
 
-			
+    return (
+        <div className="col-5 container m-5">
+            <h1> ✨Lista de Deberes📄 </h1>
+            <input
+                placeholder="nueva tarea"
+                onKeyPress={(event) => {
+                    if (event.key == "Enter") {
+                        setTarea([...tarea, { label: event.target.value, done: false }]);
+                        event.target.value = "";
+                    }
+                }}
+            />
 
-		</div>
-	);
+            {tarea.map((value, index, i) => {
+                return (
+                    <div className="container text-center">
+                        <div className="task-name" key={index}>
+                            {value.label}
+                        </div>
+                        <div className="button-container">
+                            <button
+                                className="delete-button"
+                                onClick={() => deleteTarea(index)}
+                            >
+                                🗑️
+                            </button>
+                        </div>
+                    </div>
+                );
+            })}
+            
+        </div>
+    );
 };
-
+	
+	
 export default Home;
